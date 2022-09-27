c9fff1eaf6305f7c292ba302a5f540f5d447c9c3
veneraserik123 <venera.131@mail.ru>
2022-09-27 19:50:22 -0400

    Made additional changes with AJAX and partially Cucumber

--- src/main/java/dataProviders/ConfigFileReader.java
+++ src/main/java/dataProviders/ConfigFileReader.java
@@ -76,5 +76,10 @@ public class ConfigFileReader {
         if(testDataResourcePath!= null) return testDataResourcePath;
         else throw new RuntimeException("Test Data Resource Path not specified in the Configuration.properties file for the Key:testDataResourcePath");
     }
+    public String getReportConfigPath(){
+        String reportConfigPath = properties.getProperty("reportConfigPath");
+        if(reportConfigPath!= null) return reportConfigPath;
+        else throw new RuntimeException("Report Config Path not specified in the Configuration.properties file for the Key:reportConfigPath");
+    }
 
 }
