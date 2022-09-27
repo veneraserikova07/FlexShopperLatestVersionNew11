2a44d96ddf05bba7131eb74c3e3395452e84596d
veneraserik123 <venera.131@mail.ru>
2022-09-27 18:27:27 -0400

    Added JSON Cucumber and created POJO class for new Customer-non existing  for Apply screen,

--- src/main/java/managers/FileReaderManager.java
+++ src/main/java/managers/FileReaderManager.java
@@ -1,10 +1,13 @@
 package managers;
 
 import dataProviders.ConfigFileReader;
+import dataProviders.JsonDataReader;
 
 public class FileReaderManager {
+
     private static FileReaderManager fileReaderManager = new FileReaderManager();
     private static ConfigFileReader configFileReader;
+    private static JsonDataReader jsonDataReader;
 
     private FileReaderManager() {
     }
@@ -17,4 +20,7 @@ public class FileReaderManager {
         return (configFileReader == null) ? new ConfigFileReader() : configFileReader;
     }
 
+    public JsonDataReader getJsonReader(){
+        return (jsonDataReader == null) ? new JsonDataReader() : jsonDataReader;
+    }
 }
---
+++ src/main/java/testDataTypes/Customer.java
@@ -0,0 +1,23 @@
+package testDataTypes;
+
+public class Customer {
+    public String firstName;
+    public String lastName;
+    public String phoneNumber;
+    public Address address;
+    public String DOB;
+    public String SSN;
+    public double grossIncome;
+    public String RoutingNumber;
+    public String accountNumber;
+
+
+    public class Address {
+        public String homeAddress;
+        public String city;
+        public String state;
+        public String postalCode;
+    }
+
+
+}
